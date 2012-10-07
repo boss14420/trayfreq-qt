3febeb22e05d0afdbfa448638bbafbca6bf154c5
BOSS14420 <firefoxlinux@gmail.com>
2012-10-07 15:22:31 +0700

    modified:   CMakeLists.txt 	single instance:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include <QtGui/QApplication>
+#include <QApplication>
+#include <QProcess>
+#include <QFileInfo>
+#include <QStringList>
 #include "freqtray.h"
+
+bool isRunning(QString const &executable)
+{
+    QStringList executableList(executable);
+
+    QProcess pgrep;
+    pgrep.setReadChannel (QProcess::StandardOutput);
+    pgrep.start ("pgrep", executableList);
+    pgrep.waitForFinished(300);
+
+    QByteArray pid = pgrep.readAll();
+
+    return !pid.isEmpty();
+}
 
 int main(int argc, char *argv[])
 {
+    if (isRunning (QFileInfo (argv[0]).fileName())) 
+        return 0;
+
     QApplication a(argc, argv);
 
     FreqTray::initIcon ();
