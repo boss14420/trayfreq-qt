7d3ebf6c33486cdbb8353f70e35e8a2ffa2f9364
BOSS14420 <firefoxlinux@gmail.com>
2012-10-07 15:36:26 +0700

    single instance:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QProcess>
 #include <QFileInfo>
 #include <QStringList>
+#include <QTextStream>
 #include "freqtray.h"
 
 bool isRunning(QString const &executable)
@@ -11,19 +12,25 @@ bool isRunning(QString const &executable)
     QProcess pgrep;
     pgrep.setReadChannel (QProcess::StandardOutput);
     pgrep.start ("pgrep", executableList);
-    pgrep.waitForFinished(300);
+    pgrep.waitForFinished(100);
 
-    QByteArray pid = pgrep.readAll();
+    QByteArray pids = pgrep.readAll();
+    QTextStream stream (&pids, QIODevice::ReadOnly);
 
-    return !pid.isEmpty();
+    int pid;
+    while ( !(stream >> pid).atEnd() )
+        if (pid != qApp->applicationPid())
+            return true;
+
+    return false;
 }
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+
     if (isRunning (QFileInfo (argv[0]).fileName())) 
         return 0;
-
-    QApplication a(argc, argv);
 
     FreqTray::initIcon ();
 
